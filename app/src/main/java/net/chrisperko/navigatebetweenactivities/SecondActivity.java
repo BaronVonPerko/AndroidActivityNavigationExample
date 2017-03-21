3f6d1f5966d35b43274975010762db7cc178387d
cperko <cperko@teachers-teachers.com>
2017-03-21 10:30:53 -0500

    Init

---
+++ app/src/main/java/net/chrisperko/navigatebetweenactivities/SecondActivity.java
@@ -0,0 +1,22 @@
+package net.chrisperko.navigatebetweenactivities;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        getSupportActionBar().setTitle("Second");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
